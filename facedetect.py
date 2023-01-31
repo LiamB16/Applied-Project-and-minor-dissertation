9956d85c73a0a52e8916684cff292853bf266e2c
unknown <liambryant18@gamil.com>
2023-01-31 13:39:50 +0000

    12th commit

--- facedetect.py
+++ facedetect.py
@@ -1,24 +1,24 @@
 import pathlib
 import cv2
-import numpy as npy
+
 
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
 
 clf = cv2.CascadeClassifier(str(cascade_path))
 
-camera = cv2.VideoCapture(0) # VideoCapture(number of cameras). 0 is the defualt number
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW) #captureDevice = camera # VideoCapture(number of cameras). 0 is the defualt number
 
-while (camera.isOpened()):
-    ret, frame =  camera.read(0)
-    if ret:
-         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-         faces = clf.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+while (True):#fixes issue of program crashing due to
+    ret, frame =  camera.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.CASCADE_SCALE_IMAGE
+    )
     
     for(x, y, width, height) in faces:
         cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 255, 0), 2)
@@ -27,5 +27,5 @@ while (camera.isOpened()):
     if cv2.waitKey(1) == ord("q"):
         break
     
-    camera.release()
-    cv2.destroyAllWindows
+camera.release()
+cv2.destroyAllWindows
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-_,
+(camera.isOpened())
